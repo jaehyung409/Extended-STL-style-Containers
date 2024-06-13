54a334293e679d497288e7553b992d03c46dc93b
jaehyung409 <joo3197@hanyang.ac.kr>
2024-06-13 20:30:05 +0900

    null_ptr(선언되지 않은 공간) 확인을 위한 코드 삽입 및 size, capacity 코드 재구현

--- Array/Array.h
+++ Array/Array.h
@@ -8,6 +8,13 @@
 #include <stdexcept>
 #include <algorithm>
 
+#include <type_traits>
+
+template <typename T>
+struct DefaultValue {
+    static constexpr T value = std::conditional_t<std::is_arithmetic<T>::value, std::integral_constant<T, 0>, std::integral_constant<T*, nullptr>>::value;
+};
+
 template <typename T, size_t N>
 struct Array {
     T data[N];
@@ -24,21 +31,33 @@ struct Array {
         }
         return data[index];
     }
-    int size(){ return N; }
+    int capacity(){
+        return N;
+    }
+    int size(){
+        for(const auto& element : data) {
+            if (element == DefaultValue<T>::value)
+                return &element - data;
+        }
+    }
     void print(){
-        for(const auto& element : data)
-            std::cout << element << ' ';
+        for(const auto& element : data) {
+            if(element != DefaultValue<T>::value)
+                std::cout << element << ' ';
+        }
         std::cout << '\n';
     }
     void sorting(int index){
         std::sort(data, data+index);
     }
-    int find(const T element);
-    void insert(const T element, const int index);
-    void remove(const T element);
-    int sort_find(const T element, const int low, const int high);
-    void sort_insert(const T element);
-    void sort_remove(const T element);
+    int find(T element);
+    void insert(T element, int index);
+    void remove(T element);
+    int sort_find(T element, int low, int high);
+    void sort_insert(T element);
+    void sort_remove(T element);
+    void reverse_array_extra_array();
+    void reverse_array_two_pointer();
 };
 
 
